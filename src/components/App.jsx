0d715a56b438753e34b7ecc4e4b4b552886d0afe
abiney1321 <abiney1321@gmail.com>
2025-02-14 13:03:41 +0000

    adding items to form functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { initialItems } from "../lib/constant";
 import BackgroundHeading from "./BackgroundHeading";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -5,14 +7,16 @@ import ItemList from "./ItemList";
 import Sidebar from "./Sidebar";
 
 function App() {
+  const [items, setItems] = useState(initialItems);
+
   return (
     <>
       <BackgroundHeading />
 
       <main>
         <Header />
-        <ItemList />
-        <Sidebar />
+        <ItemList items={items} />
+        <Sidebar setItems={setItems} />
       </main>
 
       <Footer />
