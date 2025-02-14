68a3474f95c8d519986c245e5bdaa8df3f81171c
abiney1321 <abiney1321@gmail.com>
2025-02-14 04:21:09 +0000

    button group refactored

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,12 +1,22 @@
 import Button from "./Button";
 
+const secondaryButtons = [
+  "Mark all as completed",
+  "Mark all as incomplete",
+  "Reset to initial",
+  "Remove all items",
+];
+
 export default function ButtonGroup() {
   return (
     <section className="button-group">
-      <Button type="secondary">Mark all as completed</Button>
-      <Button type="secondary">Mark all as incomplete</Button>
-      <Button type="secondary">Reset to initial</Button>
-      <Button type="secondary">Remove all items</Button>
+      {secondaryButtons.map((text) => {
+        return (
+          <Button key={text} type="secondary">
+            {text}
+          </Button>
+        );
+      })}
     </section>
   );
 }
