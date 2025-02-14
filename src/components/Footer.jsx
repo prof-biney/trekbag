c1a01209adad133b33b295dd20d7079fc51a39a2
abiney1321 <abiney1321@gmail.com>
2025-02-14 03:44:27 +0000

    project structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>&copy; 2025. Copyright by BlackGold</small>
+      <small>
+        Version <b>1.1</b>
+      </small>
+    </footer>
+  );
+}
